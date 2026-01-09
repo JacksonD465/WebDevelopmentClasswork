c7aa8ccc8d46fdda320a3787fe97221cc2f81ff5
JacksonD465 <jiaxud@nycstudents.net>
2026-01-09 09:40:03 -0500

    Add files via upload

---
+++ WDLesson3_3Practical/script.js
@@ -0,0 +1,10 @@
+/* Challenge 2: Create a function for each image that displays a description of the picture inside the div with an id of description */
+
+
+
+
+
+
+/* OPTIONAL Challenge 5: Make your webpage look nicer 
+by changing the color of the div when hovering over different pictures and making the title 
+different when hovering over each picture.  */
